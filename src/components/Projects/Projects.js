2f41e6608205f93ebc82cf05097c7c6b026d4a41
CarlitosDroid <carlosleonardovargashuaman@gmail.com>
2023-09-07 09:41:07 -0500

    Added a new container for the projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -4,60 +4,59 @@ import ProjectCard from "./ProjectCard";
 import jenkfast from "../../assets/jenkfast.png"
 import Particle from "../Particle";
 
-
 function Projects() {
     return (
         <Container fluid className="project-section">
             <Particle />
             <Container>
+                <h1 className="project-heading">
+                    My recent <strong className="purple">Works </strong>
+                </h1>
+                
+                <p style={{ color: "white" }}>
+                    Here are a few projects I've worked on recently.
+                </p>
+
+                <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+                    <Col md={4} className="project-card">
+                        <ProjectCard
+                            imgPath={jenkfast}
+                            title="Jenkfast"
+                            description="A simple android app"
+                            techstack="ANDROID | KOTLIN"
+                            link="https://play.google.com/store/apps/details?id=com.spidevs.jenkfast"
+                        />
+                    </Col>
+                    <Col md={4} className="project-card">
+                        <ProjectCard
+                            imgPath={jenkfast}
+                            title="Jenkfast"
+                            description="A simple android app"
+                            techstack="ANDROID | KOTLIN"
+                            link="https://play.google.com/store/apps/details?id=com.spidevs.jenkfast"
+                        />
+                    </Col>
+                    <Col md={4} className="project-card">
+                        <ProjectCard
+                            imgPath={jenkfast}
+                            title="Jenkfast"
+                            description="A simple android app"
+                            techstack="ANDROID | KOTLIN"
+                            link="https://play.google.com/store/apps/details?id=com.spidevs.jenkfast"
+                        />
+                    </Col>
+                    <Col md={4} className="project-card">
+                        <ProjectCard
+                            imgPath={jenkfast}
+                            title="Jenkfast"
+                            description="A simple android app"
+                            techstack="ANDROID | KOTLIN"
+                            link="https://play.google.com/store/apps/details?id=com.spidevs.jenkfast"
+                        />
+                    </Col>
+                </Row>
 
             </Container>
-            <h1 className="project-heading">
-                My recent <strong className="purple">Works </strong>
-            </h1>
-            <p style={{ color: "white" }}>
-                Here are a few projects I've worked on recently.
-            </p>
-
-            <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
-                <Col md={4} className="project-card">
-                    <ProjectCard
-                        imgPath={jenkfast}
-                        title="Jenkfast"
-                        description="A simple android app"
-                        techstack="ANDROID | KOTLIN"
-                        link="https://play.google.com/store/apps/details?id=com.spidevs.jenkfast"
-                    />
-                </Col>
-                <Col md={4} className="project-card">
-                    <ProjectCard
-                        imgPath={jenkfast}
-                        title="Jenkfast"
-                        description="A simple android app"
-                        techstack="ANDROID | KOTLIN"
-                        link="https://play.google.com/store/apps/details?id=com.spidevs.jenkfast"
-                    />
-                </Col>
-                <Col md={4} className="project-card">
-                    <ProjectCard
-                        imgPath={jenkfast}
-                        title="Jenkfast"
-                        description="A simple android app"
-                        techstack="ANDROID | KOTLIN"
-                        link="https://play.google.com/store/apps/details?id=com.spidevs.jenkfast"
-                    />
-                </Col>
-                <Col md={4} className="project-card">
-                    <ProjectCard
-                        imgPath={jenkfast}
-                        title="Jenkfast"
-                        description="A simple android app"
-                        techstack="ANDROID | KOTLIN"
-                        link="https://play.google.com/store/apps/details?id=com.spidevs.jenkfast"
-                    />
-                </Col>
-            </Row>
-
         </Container>
     );
 }
